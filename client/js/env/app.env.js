715dcdfe7e08ddabfb7f0293c1264a8fcbea1961
damienderoistecso <53211688+damienderoistecso@users.noreply.github.com>
2024-05-27 11:39:45 +0100

    Update app.env.js
    Sync to latest widget version 2.8.1

--- client/js/env/app.env.js
+++ client/js/env/app.env.js
@@ -5,8 +5,8 @@ Application - Environment
 const C_APP_URL_API = "https://web-service-domain.ext/";
 
 // URLs Widget
-const C_APP_URL_PXWIDGET_ISOGRAM = "https://cdn.jsdelivr.net/gh/CSOIreland/PxWidget@2.8.0/js/isogram.min.js";
-const C_APP_URL_PXWIDGET_SNIPPET = "https://cdn.jsdelivr.net/gh/CSOIreland/PxWidget@2.8.0/snippet.html";
+const C_APP_URL_PXWIDGET_ISOGRAM = "https://cdn.jsdelivr.net/gh/CSOIreland/PxWidget@2.8.1/js/isogram.min.js";
+const C_APP_URL_PXWIDGET_SNIPPET = "https://cdn.jsdelivr.net/gh/CSOIreland/PxWidget@2.8.1/snippet.html";
 
 // URLs Language Files
 const C_APP_URL_PXLANGUAGEPLUGINS = "https://cdn.jsdelivr.net/gh/CSOIreland/PxLanguagePlugins@8.0.0/client/";
