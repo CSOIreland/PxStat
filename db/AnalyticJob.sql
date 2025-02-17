fc8e4b2a12aaef2b3f285371b53abfd831159319
damiancosmoschapman <damian.chapman@cso.ie>
2025-02-17 16:58:11 +0000

    Merging 8.4.1

---
+++ db/AnalyticJob.sql
@@ -0,0 +1,165 @@
+USE [msdb]
+
+DECLARE @jId binary(16)
+DECLARE @jName NVARCHAR(500)
+	SET @jName='Update Analytics_'+ @dbname
+
+SELECT @jId = job_id FROM msdb.dbo.sysjobs WHERE (name = @jName)
+IF (@jId IS NULL)
+BEGIN
+
+	BEGIN TRANSACTION
+	DECLARE @ReturnCode INT
+	SELECT @ReturnCode = 0
+
+	IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+	BEGIN
+	EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+	END
+	
+	DECLARE @jobId BINARY(16)
+	EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=@jname, 
+			@enabled=1, 
+			@notify_level_eventlog=0, 
+			@notify_level_email=0, 
+			@notify_level_netsend=0, 
+			@notify_level_page=0, 
+			@delete_level=0, 
+			@description=N'No description available.', 
+			@category_name=N'[Uncategorized (Local)]', 
+			@owner_login_name=@dbowner, @job_id = @jobId OUTPUT
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+
+	SET @reportCommand='exec Security_Analytic_Update_ReadReport ' + char(39)  + @lngisocode + char(39) ;
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Security_Analytic_Update_ReadReport', 
+			@step_id=1, 
+			@cmdexec_success_code=0, 
+			@on_success_action=3, 
+			@on_success_step_id=0, 
+			@on_fail_action=3, 
+			@on_fail_step_id=0, 
+			@retry_attempts=0, 
+			@retry_interval=0, 
+			@os_run_priority=0, @subsystem=N'TSQL', 
+			@command=@reportCommand, --N'exec Security_Analytic_Update_ReadReport ''en''',
+			@database_name=@dbName, 
+			@flags=0
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Security_Analytic_Update_BrowserReport', 
+			@step_id=2, 
+			@cmdexec_success_code=0, 
+			@on_success_action=3, 
+			@on_success_step_id=0, 
+			@on_fail_action=3, 
+			@on_fail_step_id=0, 
+			@retry_attempts=0, 
+			@retry_interval=0, 
+			@os_run_priority=0, @subsystem=N'TSQL', 
+			@command=N'exec Security_Analytic_Update_BrowserReport', 
+			@database_name=@dbName, 
+			@flags=0
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Security_Analytic_Update_DatasetLanguage', 
+			@step_id=3, 
+			@cmdexec_success_code=0, 
+			@on_success_action=3, 
+			@on_success_step_id=0, 
+			@on_fail_action=3, 
+			@on_fail_step_id=0, 
+			@retry_attempts=0, 
+			@retry_interval=0, 
+			@os_run_priority=0, @subsystem=N'TSQL', 
+			@command=N'exec Security_Analytic_Update_DatasetLanguage', 
+			@database_name=@dbName, 
+			@flags=0
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Security_Analytic_Update_EnvironmentLanguage', 
+			@step_id=4, 
+			@cmdexec_success_code=0, 
+			@on_success_action=3, 
+			@on_success_step_id=0, 
+			@on_fail_action=3, 
+			@on_fail_step_id=0, 
+			@retry_attempts=0, 
+			@retry_interval=0, 
+			@os_run_priority=0, @subsystem=N'TSQL', 
+			@command=N'exec Security_Analytic_Update_EnvironmentLanguage', 
+			@database_name=@dbName, 
+			@flags=0
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Security_Analytic_Update_Format', 
+			@step_id=5, 
+			@cmdexec_success_code=0, 
+			@on_success_action=3, 
+			@on_success_step_id=0, 
+			@on_fail_action=3, 
+			@on_fail_step_id=0, 
+			@retry_attempts=0, 
+			@retry_interval=0, 
+			@os_run_priority=0, @subsystem=N'TSQL', 
+			@command=N'exec Security_Analytic_Update_Format', 
+			@database_name=@dbName, 
+			@flags=0
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Security_Analytic_Update_OS', 
+			@step_id=6, 
+			@cmdexec_success_code=0, 
+			@on_success_action=3, 
+			@on_success_step_id=0, 
+			@on_fail_action=3, 
+			@on_fail_step_id=0, 
+			@retry_attempts=0, 
+			@retry_interval=0, 
+			@os_run_priority=0, @subsystem=N'TSQL', 
+			@command=N'exec Security_Analytic_Update_OS', 
+			@database_name=@dbName, 
+			@flags=0
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Security_Analytic_Update_Referer', 
+			@step_id=7, 
+			@cmdexec_success_code=0, 
+			@on_success_action=1, 
+			@on_success_step_id=0, 
+			@on_fail_action=3, 
+			@on_fail_step_id=0, 
+			@retry_attempts=0, 
+			@retry_interval=0, 
+			@os_run_priority=0, @subsystem=N'TSQL', 
+			@command=N'exec Security_Analytic_Update_Referer', 
+			@database_name=@dbName, 
+			@flags=0
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=@jname, 
+			@enabled=1, 
+			@freq_type=8, 
+			@freq_interval=127, 
+			@freq_subday_type=1, 
+			@freq_subday_interval=0, 
+			@freq_relative_interval=0, 
+			@freq_recurrence_factor=1, 
+			@active_start_date=20240814, 
+			@active_end_date=99991231, 
+			@active_start_time=40000, 
+			@active_end_time=235959, 
+			@schedule_uid=N'7f306e68-5270-46cc-9a9f-cb6c2964b68d'
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	COMMIT TRANSACTION
+	GOTO EndSave
+	QuitWithRollback:
+		IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+	EndSave:
+
+END
