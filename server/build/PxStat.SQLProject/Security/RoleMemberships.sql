fcb8cf4cf3b58ef362b147be15c5b7ec50cfdbe5
neilfromcork <neilfromcork@gmail.com>
2025-02-21 11:50:46 +0000

    Split the SQL project from the main solution to its own solution

---
+++ server/build/PxStat.SQLProject/Security/RoleMemberships.sql
@@ -0,0 +1,11 @@
+﻿ALTER ROLE [db_datareader] ADD MEMBER [pxstat];
+
+
+GO
+
+ALTER ROLE [db_datawriter] ADD MEMBER [pxstat];
+
+
+GO
+
+
---
+++ server/build/PxStat.SQLProject/Security/db_executor.sql
@@ -0,0 +1,11 @@
+﻿CREATE ROLE [db_executor]
+    AUTHORIZATION [dbo];
+
+
+GO
+ALTER ROLE [db_executor] ADD MEMBER [pxstat];
+
+GO
+
+
+
